ee63702331285f0901f58c9d88c40b3bb9aeda58
condojqiOS <Hector@condojq.com>
2026-03-12 18:32:09 -0600

    Initial commit - Project setup with Swift Testing

---
+++ RecipeHub/RecipeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHubApp.swift
+//  RecipeHub
+//
+//  Created by Hector Carmona on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeHubTests/RecipeHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHubTests.swift
+//  RecipeHubTests
+//
+//  Created by Hector Carmona on 3/12/26.
+//
+
+import Testing
+@testable import RecipeHub
+
+struct RecipeHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
