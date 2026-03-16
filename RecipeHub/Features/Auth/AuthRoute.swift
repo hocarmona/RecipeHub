e914e7dd0d9de9a69724413f5cf2114702f3d7d6
hocarmonabe <orcabe86@gmail.com>
2026-03-16 13:09:10 -0600

    feat: add AuthRoute

---
+++ RecipeHub/Features/Auth/AuthRoute.swift
@@ -0,0 +1,14 @@
+//
+//  AuthRoute.swift
+//  RecipeHub
+//
+//  Created by Hector Carmona on 3/16/26.
+//
+
+import Foundation
+
+/// Auth flow routes
+enum AuthRoute: Route {
+    case login
+    case register
+}
