f9ce7cafd73b8d232d60cbcadb3935ca52d805c7
hocarmonabe <orcabe86@gmail.com>
2026-03-16 12:27:29 -0600

    feat: add AppRoute enum

---
+++ RecipeHub/App/AppRoute.swift
@@ -0,0 +1,14 @@
+//
+//  AppRoute.swift
+//  RecipeHub
+//
+//  Created by Hector Carmona on 3/16/26.
+//
+
+import Foundation
+
+/// Main app routes
+enum AppRoute: Route {
+    case auth
+    case main
+}
